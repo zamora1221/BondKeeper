856747fe429071997e84354e0e391cd6480e7e77
Enrique Zamora <72171173+zamora1221@users.noreply.github.com>
2025-09-20 09:55:58 -0500

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from .models import Tenant, Person, Indemnitor, Reference
-
-admin.site.register(Tenant)
-admin.site.register(Person)
-admin.site.register(Indemnitor)
-admin.site.register(Reference)
